017c4b77bac9eb276819a00ba9274832a47574b1
Morelatto <morelatto@protonmail.com>
2018-01-10 14:21:20 -0200

    Removed commentaries and reordered options

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,9 +1,3 @@
-# arrow up/down searches in history if line is already started
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
-
-# History Settings (big history for use with many open shells and no dups)
-# Different History files for root and standard user
 if (( ! EUID )); then
     HISTFILE=$ZSH_CACHE/history_root
 else
@@ -13,31 +7,19 @@ fi
 SAVEHIST=10000
 HISTSIZE=12000
 
-setopt share_history append_history extended_history hist_no_store hist_ignore_all_dups hist_ignore_space
+setopt SHARE_HISTORY 
+setopt APPEND_HISTORY 
+setopt EXTENDED_HISTORY 
+setopt HIST_NO_STORE 
+setopt HIST_IGNORE_ALL_DUPS 
+setopt HIST_IGNORE_SPACE
 
-# If a command is issued that can’t be executed as a normal command, and the command is the name of a directory, perform the cd command to that directory.
 setopt AUTO_CD
-
-# Treat  the ‘#’, ‘~’ and ‘^’ characters as part of patterns for filename generation, etc.  (An initial unquoted ‘~’ always produces named directory expansion.)
-setopt EXTENDED_GLOB
-
-# If a pattern for filename generation has no matches, print an error, instead of leaving it unchanged in the argument  list. This also applies to file expansion of an initial ‘~’ or ‘=’.
-setopt NOMATCH
-
-# no Beep on error in ZLE.
-setopt NO_BEEP
-
-# Remove  any right prompt from display when accepting a command line. This may be useful with terminals with other cut/paste methods.
-setopt TRANSIENT_RPROMPT
-
-# If unset, the cursor is set to the end of the word if completion is started. Otherwise it stays there and completion is done from both ends.
-setopt COMPLETE_IN_WORD
-
-# Make cd push the old directory onto the directory stack.
 setopt AUTO_PUSHD
-
-# Don’t push multiple copies of the same directory onto the directory stack.
 setopt PUSHD_IGNORE_DUPS
-
-# DON NOT Allow ‘>’ redirection to truncate existing files, and ‘>>’ to create files. Otherwise ‘>!’ or ‘>|’ must be used to truncate  a file, and ‘>>!’ or ‘>>|’ to create a file.
+setopt EXTENDED_GLOB
+setopt NOMATCH
+setopt NO_BEEP
+setopt TRANSIENT_RPROMPT
+setopt COMPLETE_IN_WORD
 setopt NO_CLOBBER
