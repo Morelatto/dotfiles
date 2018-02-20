1351d5ce4fa78053c5614e023750150bb4ac017b
Morelatto <morelatto@protonmail.com>
2018-02-20 18:38:15 -0300

    Fixed zsh function and prompt loading

--- zsh/functions/functions.zsh
+++ zsh/functions/functions.zsh
@@ -1,3 +1,6 @@
 autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
+
+source $ZFUNCTIONS/vi-mode.zsh
+source $ZFUNCTIONS/zsh-syntax-highlighting.zsh
