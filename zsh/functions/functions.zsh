c9a678d7556493c1672b9cb6d44b2575a7b893e9
Morelatto <morelatto@protonmail.com>
2018-04-04 17:06:18 -0300

    Added zsh time functions

--- zsh/functions/functions.zsh
+++ zsh/functions/functions.zsh
@@ -4,6 +4,7 @@ zle -N history-beginning-search-forward-end history-search-end
 
 source $ZFUNCTIONS/vi-mode.zsh
 source $ZFUNCTIONS/expand-aliases.zsh
+source $ZFUNCTIONS/time-functions.zsh
 source $ZFUNCTIONS/zsh-autosuggestions.zsh
 source $ZFUNCTIONS/history-search-multi-word/history-search-multi-word.plugin.zsh
 source /usr/share/autojump/autojump.zsh
