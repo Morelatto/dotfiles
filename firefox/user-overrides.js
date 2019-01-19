a7736b00e860a46b946ac7e67a336e9e9ece6cbc
Morelatto <morelatto@protonmail.com>
2019-01-19 19:56:57 -0200

    Updated user-overrides.js.

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -10,11 +10,8 @@ user_pref("browser.urlbar.suggest.bookmark", true);             // allow auto-co
 user_pref("browser.urlbar.suggest.openpage", true);             // allow auto-complete of text entered in the address bar
 user_pref("browser.download.forbid_open_with", false);          // allow the `open with` option when downloading a file
 user_pref("browser.sessionstore.max_tabs_undo", 10);            // allow to restore closed tabs
-user_pref("browser.sessionstore.max_windows_undo", 10);         // allow to restore closed windows
-user_pref("full-screen-api.enabled", true);                     // enable full-screen video capability
 user_pref("layout.spellcheckDefault", 2);                       // enable spell-check for single line inputs
 user_pref("signon.rememberSignons", false);                     // do not remember logins and passwords
-user_pref("browser.sessionstore.resume_from_crash", true);      // enable resuming session from crash
 user_pref("dom.allow_cut_copy", true);                          // enables clipboard commands
 
 // PERSONAL
@@ -23,6 +20,7 @@ user_pref("browser.tabs.warnOnCloseOtherTabs", false);
 user_pref("browser.tabs.warnOnOpen", false);
 user_pref("full-screen-api.warning.delay", 0);
 user_pref("full-screen-api.warning.timeout", 0);
+user_pref("general.warnOnAboutConfig", false);
 user_pref("browser.download.autohideButton", true);
 user_pref("layout.spellcheckDefault", 2);
 
