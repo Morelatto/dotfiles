e9e8126db8be8bcc2c3fd9a300a80df245bfe20a
Morelatto <morelatto@pm.me>
2019-04-28 16:38:02 -0300

    Updated and added aliases.

--- zsh/aliases/aliases.zsh
+++ zsh/aliases/aliases.zsh
@@ -1,6 +1,6 @@
 typeset -ga asources
 asources+="$ZALIASES/common.zsh"
-asources+="$ZALIASES/work.zsh"
+asources+="$ZALIASES/arch.zsh"
 
 foreach file (`echo $asources`)
     if [[ -a $file ]]; then
