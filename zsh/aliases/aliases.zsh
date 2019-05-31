ca6395a9c508faae1d19c08d3c75134962f8e03a
Morelatto <morelatto@protonmail.com>
2019-05-31 17:30:36 -0300

    Updated zsh aliases and added new bundles. Updated README.

--- zsh/aliases/aliases.zsh
+++ zsh/aliases/aliases.zsh
@@ -1,9 +1,1 @@
-typeset -ga asources
-asources+="$ZALIASES/common.zsh"
-asources+="$ZALIASES/arch.zsh"
-
-foreach file (`echo $asources`)
-    if [[ -a $file ]]; then
-        source $file
-    fi
-end
+source "$ZALIASES/common.zsh"
