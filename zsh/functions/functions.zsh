8ab1841152bd35525e9a33d095c99413a8daab6d
Morelatto <morelatto@protonmail.com>
2019-05-31 16:07:20 -0300

    Replaced manual zsh plugin installation by antigen. Removed zsh plugins submodules. Updated README.

--- zsh/functions/functions.zsh
+++ zsh/functions/functions.zsh
@@ -2,11 +2,5 @@ autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 
-source $ZFUNCTIONS/vi-mode.zsh
 source $ZFUNCTIONS/expand-aliases.zsh
 source $ZFUNCTIONS/time-functions.zsh
-source $ZFUNCTIONS/zsh-autosuggestions.zsh
-source $ZFUNCTIONS/history-search-multi-word/history-search-multi-word.plugin.zsh
-source $ZFUNCTIONS/autojump.zsh
-source $ZFUNCTIONS/zsh-completions/zsh-completions.plugin.zsh
-source $ZFUNCTIONS/zsh-syntax-highlighting.zsh
