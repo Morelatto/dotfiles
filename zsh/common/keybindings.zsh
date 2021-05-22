cd106c238e205275b0c46bca3fdd0dc24379832a
Pedro Morelatto <pedro.michilis@willbank.com.br>
2021-05-22 17:22:33 -0300

    Adding zplug, updating aliases, functions and plugins.

--- zsh/common/keybindings.zsh
+++ zsh/common/keybindings.zsh
@@ -15,23 +15,27 @@ key[Right]="${terminfo[kcuf1]}"
 key[PageUp]="${terminfo[kpp]}"
 key[PageDown]="${terminfo[knp]}"
 key[Shift-Tab]="${terminfo[kcbt]}"
+key[Control-Delete]="${terminfo[kDC5]}"
+key[Control-Left]="${terminfo[kLFT5]}"
+key[Control-Right]="${terminfo[kRIT5]}"
 
 # setup key accordingly
-[[ -n "${key[Home]}"        ]] && bindkey -- "${key[Home]}"         beginning-of-line
-[[ -n "${key[End]}"         ]] && bindkey -- "${key[End]}"          end-of-line
-[[ -n "${key[Insert]}"      ]] && bindkey -- "${key[Insert]}"       overwrite-mode
-[[ -n "${key[Backspace]}"   ]] && bindkey -- "${key[Backspace]}"    backward-delete-char
-[[ -n "${key[Delete]}"      ]] && bindkey -- "${key[Delete]}"       delete-char
-[[ -n "${key[Up]}"          ]] && bindkey -- "${key[Up]}"           up-line-or-history
-[[ -n "${key[Down]}"        ]] && bindkey -- "${key[Down]}"         down-line-or-history
-[[ -n "${key[Left]}"        ]] && bindkey -- "${key[Left]}"         backward-char
-[[ -n "${key[Right]}"       ]] && bindkey -- "${key[Right]}"        forward-char
-[[ -n "${key[PageUp]}"      ]] && bindkey -- "${key[PageUp]}"       beginning-of-buffer-or-history
-[[ -n "${key[PageDown]}"    ]] && bindkey -- "${key[PageDown]}"     end-of-buffer-or-history
-[[ -n "${key[Shift-Tab]}"   ]] && bindkey -- "${key[Shift-Tab]}"    reverse-menu-complete
-[[ -n "${key[Ctrl-Delete]}" ]] && bindkey -- "${key[Ctrl-Delete]}"  kill-word
-[[ -n "${key[Ctrl-Right]}"  ]] && bindkey -- "${key[Ctrl-Right]}"   forward-word
-[[ -n "${key[Ctrl-Left]}"   ]] && bindkey -- "${key[Ctrl-Left]}"    backward-word
+[[ -n "${key[Home]}"        	]] && bindkey -- "${key[Home]}"         	beginning-of-line
+[[ -n "${key[End]}"         	]] && bindkey -- "${key[End]}"          	end-of-line
+[[ -n "${key[Insert]}"      	]] && bindkey -- "${key[Insert]}"       	overwrite-mode
+[[ -n "${key[Backspace]}"   	]] && bindkey -- "${key[Backspace]}"    	backward-delete-char
+[[ -n "${key[Delete]}"      	]] && bindkey -- "${key[Delete]}"       	delete-char
+[[ -n "${key[Up]}"          	]] && bindkey -- "${key[Up]}"           	up-line-or-history
+[[ -n "${key[Down]}"        	]] && bindkey -- "${key[Down]}"         	down-line-or-history
+[[ -n "${key[Left]}"        	]] && bindkey -- "${key[Left]}"         	backward-char
+[[ -n "${key[Right]}"       	]] && bindkey -- "${key[Right]}"        	forward-char
+[[ -n "${key[PageUp]}"      	]] && bindkey -- "${key[PageUp]}"       	beginning-of-buffer-or-history
+[[ -n "${key[PageDown]}"    	]] && bindkey -- "${key[PageDown]}"     	end-of-buffer-or-history
+[[ -n "${key[Shift-Tab]}"   	]] && bindkey -- "${key[Shift-Tab]}"    	reverse-menu-complete
+[[ -n "${key[Alt-Backspace]}" 	]] && bindkey -- "${key[Alt-Backspace]}"  	backward-kill-word
+[[ -n "${key[Ctrl-Delete]}" 	]] && bindkey -- "${key[Ctrl-Delete]}"  	kill-word
+[[ -n "${key[Ctrl-Right]}"  	]] && bindkey -- "${key[Ctrl-Right]}"   	forward-word
+[[ -n "${key[Ctrl-Left]}"   	]] && bindkey -- "${key[Ctrl-Left]}"    	backward-word
 
 # Finally, make sure the terminal is in application mode, when zle is
 # active. Only then are the values from $terminfo valid.
