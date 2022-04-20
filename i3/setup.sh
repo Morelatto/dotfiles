3f8843caa2491c214ab669466154ed3ffd9e0e19
Pedro Morelatto <morelatto@pm.me>
2022-04-19 23:58:49 -0300

    Updates i3 configuration and adds dunst

--- i3/setup.sh
+++ i3/setup.sh
@@ -6,15 +6,12 @@ I3_CONFIG_DIR=$CONFIG_DIR/i3
 I3_BLOCKS_CONFIG_DIR=$CONFIG_DIR/i3blocks
 
 sudo pacman -S xorg-server xorg-apps xorg-xinit
-sudo pacman -S i3-gaps i3blocks i3lock rofi
+sudo pacman -S i3-gaps i3blocks i3lock rofi dunst
 
-# i3
 mkdir -p $I3_CONFIG_DIR
 ln -sf $DOTFILES_DIR/config $I3_CONFIG_DIR/config
 
-# i3blocks
 git clone https://github.com/vivien/i3blocks-contrib $I3_BLOCKS_CONFIG_DIR
 ln -sf $DOTFILES_DIR/i3blocks.config $I3_BLOCKS_CONFIG_DIR/config
 
-# rofi
-yay -Si rofi-dmenu
+yay -Si rofi-dmenu paper-icon-theme
