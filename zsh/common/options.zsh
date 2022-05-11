6956a454dbae5e105664d5aa2be7b841086928f3
Pedro Morelatto <morelatto@pm.me>
2022-05-11 03:02:12 -0300

    Updates zsh config

--- zsh/common/options.zsh
+++ zsh/common/options.zsh
@@ -2,14 +2,12 @@
 # zsh 5.8.1
 # man 1 zshoptions
 
-
 export HISTFILE="$ZSH_CACHE/.zhistory"
 SAVEHIST=1000
 HISTSIZE=1500
 
-
-setopt share_history hist_ignore_space
-setopt glob_dots complete_in_word no_nomatch glob_subst no_bad_pattern
+setopt share_history hist_ignore_space complete_in_word
+setopt menu_complete glob_dots no_nomatch no_bad_pattern
 setopt auto_cd auto_pushd pushd_ignore_dups pushd_to_home
-unsetopt glob_complete complete_aliases list_ambiguous beep
+unsetopt glob_complete complete_aliases beep
 
