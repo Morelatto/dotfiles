c2c587ca02cb2906be8a98d7e6eae7a1814af4f2
Pedro Morelatto <morelatto@pm.me>
2022-05-27 00:02:16 -0300

    Updates zsh menu and kill completion

--- zsh/common/completions.zsh
+++ zsh/common/completions.zsh
@@ -6,7 +6,9 @@ zstyle ':completion:*::::' completer _expand _complete _ignored _approximate
 zstyle -e ':completion:*:approximate:*' max-errors 'reply=( $(( ($#PREFIX+$#SUFFIX)/3 )) numeric )'
 #zstyle ':completion:*:expand:*' tag-order all-expansions
 zstyle ':completion:*' verbose yes
-zstyle ':completion:*' menu select
+zstyle ':completion:*' menu select=2 eval "$(dircolors -b)"
+#zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:descriptions' format '%B%d%b'
 zstyle ':completion:*:messages' format '%d'
 zstyle ':completion:*:warnings' format 'No matches for: %d'
@@ -15,6 +17,7 @@ zstyle ':completion:*' group-name ''
 zstyle ':completion:*' list-dirs-first true
 #zstyle ':completion:*' accept-exact-dirs true
 zstyle ':completion:*' matcher-list 'r:|=*' 'l:|=* r:|=*'
-zstyle ':completion:*' list-colors ''
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
 
 zmodload zsh/complist
