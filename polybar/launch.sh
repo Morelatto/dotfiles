69b6cdce85ca567aba45140c9aa9700f7b4522af
Pedro Morelatto <morelatto@pm.me>
2022-06-01 09:58:35 -0300

    Updates polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,7 +2,7 @@
 
 # Add this script to your wm startup file.
 
-DIR="$HOME/.config/polybar/material"
+DIR="$HOME/.config/polybar/grayblocks"
 
 # Terminate already running bar instances
 killall -q polybar
@@ -10,6 +10,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Package id 0" ]; then
+        export HWMON_PATH_CPU="$i"
+    elif [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "nvme: Composite" ]; then
+        export HWMON_PATH_NVME="$i"
+    fi
+done
+
 # Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+polybar -q main1 -c "$DIR"/config.ini &
 polybar -q main2 -c "$DIR"/config.ini &
+
