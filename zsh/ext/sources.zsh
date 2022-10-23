fd377f9e6d2396f4859f81b0894dc71ca4287420
Pedro Morelatto <morelatto@pm.me>
2022-10-23 14:07:58 -0300

    Adds zsh plugin for command time and alias tips

--- zsh/ext/sources.zsh
+++ zsh/ext/sources.zsh
@@ -103,6 +103,45 @@ source $DOTDIR/zsh/ext/zsh-histdb-skim/zsh-histdb-skim.zsh
 
 export HISTDB_SKIM_PATH=${HOME}/.local/share/zsh-histdb-skim
 
+# ================
+# zsh-command-time
+# ================
+
+# https://github.com/popstas/zsh-command-time
+
+source $DOTDIR/zsh/ext/zsh-command-time/command-time.plugin.zsh
+
+export ZSH_COMMAND_TIME_MIN_SECONDS=10
+
+zsh_command_time() {
+    if [ -n "$ZSH_COMMAND_TIME" ]; then
+        hours=$(($ZSH_COMMAND_TIME/3600))
+        min=$(($ZSH_COMMAND_TIME/60))
+        sec=$(($ZSH_COMMAND_TIME%60))
+        if [ "$ZSH_COMMAND_TIME" -le 60 ]; then
+            timer_show="$fg[green]$ZSH_COMMAND_TIME s"
+        elif [ "$ZSH_COMMAND_TIME" -gt 60 ] && [ "$ZSH_COMMAND_TIME" -le 180 ]; then
+            timer_show="$fg[yellow]$min min. $sec s"
+        else
+            if [ "$hours" -gt 0 ]; then
+                min=$(($min%60))
+                timer_show="$fg[red]$hours h. $min min. $sec s"
+            else
+                timer_show="$fg[red]$min min. $sec s"
+            fi
+        fi
+        printf "${ZSH_COMMAND_TIME_MSG}\n" "$timer_show"
+    fi
+}
+
+# ==========
+# alias-tips
+# ==========
+
+# https://github.com/djui/alias-tips
+
+source $DOTDIR/zsh/ext/alias-tips/alias-tips.plugin.zsh
+
 # ========
 # autojump
 # ========
