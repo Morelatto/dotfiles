e83a1b57a6219e9c4b06c027801e5aab32f4be08
Morelatto <pedro.michilis@willbank.com.br>
2023-08-26 01:10:35 -0300

    fix(sct.sh): fix variable names PART and WIND to PARTIAL and WINDOW respectively for better readability and semantics feat(sct.sh): add support for saving screenshots in a specific directory and with a timestamped filename for better organization

--- scripts/sct.sh
+++ scripts/sct.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 
-SCREENSHOTER="maim"
-TIMESTAMP=$(date +%F_%H-%M-%S)
-SAVE_LOCATION=~/Pictures
-SAVE_FILE="$SAVE_LOCATION/$TIMESTAMP-Screenshot.png"
+SCT_ARGS="maim"
+PARTIAL=0
+WINDOW=0
 
 screenshot() {
-    if [ "$PART" -gt 0 ]; then
-        SCREENSHOTER+=" -s"
-    elif [ "$WIND" -gt 0 ]; then
-        SCREENSHOTER+=" -i \$(xdotool getactivewindow)"
+    SAVE_LOCATION=$HOME/Pictures/Screenshot
+    SAVE_FILE="$(date +%F_%H-%M-%S)-Screenshot.png"
+    
+    if [ $PARTIAL -gt 0 ]; then
+        SCT_ARGS="$SCT_ARGS -s"
+    elif [ $WINDOW -gt 0 ]; then
+        SCT_ARGS+=" -i \$(xdotool getactivewindow)"
     fi
-    eval $SCREENSHOTER | tee $SAVE_FILE | xclip -sel clip -t image/png
+    eval $SCT_ARGS | tee "$SAVE_LOCATION/$SAVE_FILE" | xclip -sel clip -t image/png
 }
 
 case $1 in
-    "partial") PART=1;;
+    "partial") PARTIAL=1;;
     "full") FULL=1;;
-    "window") WIND=1;;
+    "window") WINDOW=1;;
 esac
 
 screenshot
